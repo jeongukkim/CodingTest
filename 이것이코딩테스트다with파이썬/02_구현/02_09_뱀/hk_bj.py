a879f77c46aa78fd5e3d367880a6fc3a4528c0c3
tkfka1 <36651040+tkfka1@users.noreply.github.com>
2023-06-16 15:01:17 +0900

    616

--- 이것이코딩테스트다with파이썬/02_구현/02_09_뱀/hk_bj.py
+++ 이것이코딩테스트다with파이썬/02_구현/02_09_뱀/hk_bj.py
@@ -1,34 +1,20 @@
 from collections import deque
-
-def solution(n,k,apple,l,info):
-    ### 가정 : 중복된 시간으로 방향을 바꾸는 경우는 없다.
-
-    ## n = 보드의 크기
-    ## k = 사과의 개수
-    ## apple = 사과의 위치
-    ## l = 뱀의 방향 변환 횟수
-    ## info = 뱀의 방향 변환 정보
-
-    ## 보드 만들기
-    li = [[0]*n for i in range(n)]
-
-    ## 사과 보드에 넣기
-    for i in apple:
-        a,b = map(int,i.split())
-        li[a-1][b-1] = 1
+n = int(input()) ## n = 보드의 크기
+k = int(input()) ## k = 사과의 개수
+li = [[0]*n for i in range(n)] ## li = 사과가 있는 보드
+for i in range(k):
+    a,b = map(int, input().split())
+    li[a-1][b-1] = 1
     
-    # print(*li, sep='\n')
+l = int(input()) ## l = 뱀의 방향 변환 횟수
+info_dic = {} ## info_dic = 뱀의 방향 변환 정보 딕셔너리
+for i in range(l):
+    a,b = input().split()
+    print(a,b)
+    info_dic[int(a)] = b
 
-    ## 방향 변환 정보 딕셔너리 info_dic[시간] = 방향
-    info_dic = {}
-    for i in info:
-        a,b = i.split()
-        a = int(a)
-        info_dic[a] = b
-
-    ## 뱀의 위치, 방향, 시작 시간
-    ## 오른쪽으로 한칸가놓고 시작은 1초로 한다.
-
+### 가정 : 중복된 시간으로 방향을 바꾸는 경우는 없다.
+def sol(n,k,li,l,info_dic):
     ## [direction] 0 = 우, 1 = 하, 2 = 좌, 3 = 상
     dx = [1, 0, -1, 0]
     dy = [0, 1, 0, -1]
@@ -42,7 +28,7 @@ def solution(n,k,apple,l,info):
     elif info_dic.get(0) == 'L':
         ## 왼쪽으로가면 위에 쳐박음
         return 1
-    
+
     time = 0  # 현재 시간
 
     while True:
@@ -54,7 +40,7 @@ def solution(n,k,apple,l,info):
         ## 박히거나 내 몸에 부딪히면 끝
         if nx < 0 or nx >= n or ny < 0 or ny >= n or (ny, nx) in snake:
             return time
-        
+
         # 다음 위치에 사과가 있는지 확인
         if li[ny][nx] == 1:
             li[ny][nx] = 0  # 사과 제거
@@ -62,7 +48,7 @@ def solution(n,k,apple,l,info):
         else:
             snake.appendleft((ny, nx))  # 뱀의 머리 이동
             snake.pop()  # 꼬리 제거
-        
+
         ## 방향 전환
         if info_dic.get(time) == 'D':
             direction += 1
@@ -72,10 +58,5 @@ def solution(n,k,apple,l,info):
             direction -= 1
             if direction == -1:
                 direction = 3
-
-print(solution(6,3,["3 4",
-                    "2 5",
-                    "5 3"],
-                    3,["3 D",
-                    "15 L",
-                    "17 D"]))
+                
+print(sol(n,k,li,l,info_dic))
