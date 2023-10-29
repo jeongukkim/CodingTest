9cbf6430d34ea82c447b03ac7f2f1f48d5054e7f
jeongukkim <79093912+jeongukkim@users.noreply.github.com>
2023-10-29 22:36:34 +0900

    [Gold IV] Title: 뱀, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/3190. 뱀/뱀.py
@@ -0,0 +1,56 @@
+n = int(input())
+
+num_apples = int(input())
+data = [[0] * (n+1) for _ in range(n+1)]
+for _ in range(num_apples):
+    y, x = map(int, input().split())
+    data[y][x] = 1
+
+l = int(input())
+info = []
+for _ in range(l):
+    x, c = input().split()
+    info.append((int(x), c))
+
+dy = [0, 1, 0, -1]
+dx = [1, 0, -1, 0]
+
+def turn(direction, c):
+    if c == 'L':
+        direction = (direction - 1) % 4
+    else:
+        direction = (direction + 1) % 4
+    return direction
+
+def simulate():
+    x, y = 1, 1
+    data[x][y] = 2
+    direction = 0
+    time = 0
+    index = 0
+    q = [(x, y)]
+    
+    while True:
+        nx = x + dx[direction]
+        ny = y + dy[direction]
+        if 1 <= nx <= n and 1 <= ny <= n and data[ny][nx] != 2:
+            if data[ny][nx] == 0:
+                data[ny][nx] = 2
+                q.append((nx, ny))
+                px, py = q.pop(0)
+                data[py][px] = 0
+            if data[ny][nx] == 1:
+                data[ny][nx] = 2
+                q.append((nx, ny))
+        else:
+            time += 1
+            break
+        
+        x, y = nx, ny
+        time += 1
+        if index < l and time == info[index][0]:
+            direction = turn(direction, info[index][1])
+            index += 1
+    return time
+
+print(simulate())
