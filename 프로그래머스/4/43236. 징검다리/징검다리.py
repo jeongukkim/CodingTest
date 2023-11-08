e48301c63a2396a21e682d5a271bb4975afe2414
jeongukkim <79093912+jeongukkim@users.noreply.github.com>
2023-11-08 23:47:19 +0900

    [level 4] Title: 징검다리, Time: 96.49 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/4/43236. 징검다리/징검다리.py
+++ 프로그래머스/4/43236. 징검다리/징검다리.py
@@ -1,6 +1,3 @@
-INF = 1e9
-
-
 def solution(distance, rocks, n):
     answer = 0
     
@@ -13,14 +10,12 @@ def solution(distance, rocks, n):
     while start <= end:
         mid = (start + end) // 2
         
-        min_distance = int(INF)
         num_removed_rocks = 0
         value = 0
         for rock in rocks:
             if rock < value + mid:
                 num_removed_rocks += 1
             else:
-                min_distance = min(min_distance, rock - value)
                 value = rock
         
         if num_removed_rocks <= n:
